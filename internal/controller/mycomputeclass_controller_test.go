805be3da22d187285d589e2013e0964bb1da67d6
tryuuu <ryu23210@gmail.com>
2025-01-15 10:39:44 +0900

    test

--- internal/controller/mycomputeclass_controller_test.go
+++ internal/controller/mycomputeclass_controller_test.go
@@ -1,84 +1,101 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"sort"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	scalingv1 "tryu.com/my-computeclass/api/v1"
 )
 
-var _ = Describe("MyComputeClass Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+// PrioritySortingTest では、MyComputeClassReconciler の HandlePriorityList が
+// MyComputeClass.Spec.Properties を正しく Priority 順にソートするかを検証します。
+var _ = Describe("MyComputeClass Priority Sorting", func() {
+	const (
+		testNamespace = "default"
+		resourceName  = "test-mycomputeclass-priority"
+	)
 
-		ctx := context.Background()
+	ctx := context.Background()
+	typeNamespacedName := types.NamespacedName{
+		Namespace: testNamespace,
+		Name:      resourceName,
+	}
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+	BeforeEach(func() {
+		// EnvTest 上に同名のリソースが残っていれば削除しておく
+		mc := &scalingv1.MyComputeClass{}
+		err := k8sClient.Get(ctx, typeNamespacedName, mc)
+		if err == nil {
+			_ = k8sClient.Delete(ctx, mc)
+		} else if !k8serrors.IsNotFound(err) {
+			// それ以外のエラーであればテストを中断
+			Expect(err).NotTo(HaveOccurred())
 		}
-		mycomputeclass := &scalingv1.MyComputeClass{}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind MyComputeClass")
-			err := k8sClient.Get(ctx, typeNamespacedName, mycomputeclass)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &scalingv1.MyComputeClass{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
+		// 新規 MyComputeClass リソースを作成
+		// Priority がバラバラの順序で並んでいる
+		testMC := &scalingv1.MyComputeClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      resourceName,
+				Namespace: testNamespace,
+			},
+			Spec: scalingv1.MyComputeClassSpec{
+				Properties: []scalingv1.InstanceProperty{
+					{MachineFamily: "n2", Priority: 3},
+					{MachineFamily: "e2", Priority: 2},
+					{MachineFamily: "n1", Priority: 1},
+				},
+			},
+		}
+		err = k8sClient.Create(ctx, testMC)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		// 後片付けとして、テスト用 MyComputeClass を削除
+		mc := &scalingv1.MyComputeClass{}
+		err := k8sClient.Get(ctx, typeNamespacedName, mc)
+		if err == nil {
+			_ = k8sClient.Delete(ctx, mc)
+		}
+	})
+
+	It("should return a Priority-sorted list via HandlePriorityList", func() {
+		// Reconciler 作成 (GKEClient や他のフィールドは今回は使わない想定)
+		r := &MyComputeClassReconciler{
+			Client: k8sClient,
+			Scheme: k8sClient.Scheme(),
+		}
+
+		// Reconcile Request を準備
+		req := reconcile.Request{
+			NamespacedName: typeNamespacedName,
+		}
+
+		// HandlePriorityList を直接呼んで Priority ソートされるかを確認
+		sortedProps, err := r.HandlePriorityList(ctx, req)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(sortedProps).To(HaveLen(3))
+
+		// 返されたスライスが Priority の昇順であることを検証
+		// (sortedProps[0].Priority <= sortedProps[1].Priority <= sortedProps[2].Priority)
+		isSorted := sort.SliceIsSorted(sortedProps, func(i, j int) bool {
+			return sortedProps[i].Priority < sortedProps[j].Priority
 		})
+		Expect(isSorted).To(BeTrue(), "Properties should be sorted by ascending Priority")
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &scalingv1.MyComputeClass{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance MyComputeClass")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &MyComputeClassReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
+		// 具体的に Priority 値が 1,2,3 の順かも確認
+		Expect(sortedProps[0].MachineFamily).To(Equal("n1"))
+		Expect(sortedProps[1].MachineFamily).To(Equal("e2"))
+		Expect(sortedProps[2].MachineFamily).To(Equal("n2"))
 	})
 })
--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -18,9 +18,7 @@ package controller
 
 import (
 	"context"
-	"fmt"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -67,8 +65,8 @@ var _ = BeforeSuite(func() {
 		// default path defined in controller-runtime which is /usr/local/kubebuilder/.
 		// Note that you must have the required binaries setup under the bin directory to perform
 		// the tests directly. When we run make test it will be setup and used automatically.
-		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
-			fmt.Sprintf("1.31.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
+		// 	BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
+		// 		fmt.Sprintf("1.31.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
 	}
 
 	var err error
